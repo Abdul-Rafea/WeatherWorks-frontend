768a062b078d5b67dd74fa021f1c854aecc8d55b
Abdul Rafea <abdulrafea97@gmail.com>
2025-12-13 02:57:37 +0500

    .

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+console.log('--- VERCEL ENV CHECK START ---');
+console.log('VITE_WEATHER_API_KEY is available:', process.env.VITE_WEATHER_API_KEY ? 'YES' : 'NO');
+console.log('--- VERCEL ENV CHECK END ---');
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
