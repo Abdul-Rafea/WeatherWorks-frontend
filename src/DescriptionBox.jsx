86a87627ea30f500c3ab81ae6954a19db9e55dc3
Abdul Rafea <abdulrafea97@gmail.com>
2026-01-19 16:50:16 +0500

    fully responsive landing page

--- src/DescriptionBox.jsx
+++ src/DescriptionBox.jsx
@@ -5,13 +5,13 @@ function DescriptionBox(props){
         grey: "bg-[#E7E7E7]", 
     }
     return(
-        <div className={`w-2/3 ${colorVar[props.color]} flex justify-center items-center flex-col p-3 rounded-2xl`}>
+        <div className={`w-2/3 ${colorVar[props.color]} flex justify-center items-center flex-col p-3 rounded-2xl sm:w-1/2 xl:w-1/3`}>
             {props.icon}
-            <h2 className="text-xl font-iceberg">{props.heading}</h2>
+            <h2 className="w-full text-xl font-iceberg text-center sm:text-2xl">{props.heading}</h2>
             {props.heading2 && (
-                <h3 className="text-lg font-iceberg">{props.heading2}</h3>
+                <h3 className="w-full text-lg text-center font-iceberg sm:text-2xl">{props.heading2}</h3>
             )}
-            <p className="text-base font-sans text-midGreen text-wrap text-center"><q>{props.description}</q></p>
+            <p className="w-full text-base font-sans text-center text-midGreen text-wrap text-center text-lg"><q>{props.description}</q></p>
         </div>
     );
 }
